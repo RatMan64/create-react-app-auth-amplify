91f20c8ac5d71a3db00afda7e6cdbd492aac4c71
RileyRat5x <35906066+RileyRat5x@users.noreply.github.com>
2021-11-13 13:05:07 -0800

    redo

--- src/App.js
+++ src/App.js
@@ -8,7 +8,29 @@ Amplify.configure(aws_exports);
 
 class App extends Component {
   render() {
-    return null;
+
+
+
+
+    return (
+      <div className="App">
+        <AmplifySignOut />
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
   }
 }
 
