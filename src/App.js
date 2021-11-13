03b0f238151640dfe0abbf44962aba05813dec7a
RileyRat5x <35906066+RileyRat5x@users.noreply.github.com>
2021-11-13 13:35:47 -0800

    looking at if the user email will be printed out ot console

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import { Auth } from 'aws-amplify';
 import Amplify from 'aws-amplify';
 import aws_exports from './aws-exports';
 Amplify.configure(aws_exports);
 
 class App extends Component {
   render() {
+    console.log(Auth.user.attributes.email)
     return (
       <div className="App">
         <AmplifySignOut />
