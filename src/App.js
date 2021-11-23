88c4827dfb680ba91a1020c6571e656e60e618c2
RileyRat5x <35906066+RileyRat5x@users.noreply.github.com>
2021-11-22 17:00:48 -0800

    getrandp makes a random password however doesnt work with the visual elements

--- src/App.js
+++ src/App.js
@@ -9,15 +9,21 @@ import Amplify from 'aws-amplify';
 import aws_exports from './aws-exports';
 Amplify.configure(aws_exports);
 
-class App extends Component {
+class App extends Component{
   render() {
     var ValueInP =""
 
     console.log(Auth.user.attributes)
     function GetRandP(){
+      var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      var passwordlen = 9
       console.log("this button was pushed" )
-      var randpassword = "this sucks"
-      ValueInP = randpassword
+      var randpassword = ""
+      for (var i = 0; i<=passwordlen; i++){
+        var randnum = Math.floor(Math.random() * chars.length)
+        randpassword += chars.substring(randnum, randnum +1)
+      }
+      console.log(randpassword)
     }
 
     return (
@@ -25,7 +31,7 @@ class App extends Component {
         <AmplifySignOut />
         <Creator />
         <input type="text"/>
-        <input name="password" onChange={ValueInP} value={ValueInP} type="text"/>
+        <input name="password"  type="text"/>
 
         <button onClick={GetRandP}>create random</button>
         <button>add password</button>
@@ -37,6 +43,7 @@ class App extends Component {
     );
   }
 }
+
 
 
 
