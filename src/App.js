d7d5ba6f224b0abfeb93f3d56367d645ba287e7c
RileyRat5x <35906066+RileyRat5x@users.noreply.github.com>
2021-12-04 18:55:41 -0800

    random password can be created

--- src/App.js
+++ src/App.js
@@ -19,66 +19,17 @@ class App extends Component{
 
     constructor() {
         super();
-
-        this.usenum = React.createRef()
-        this.GetRandP = this.GetRandP.bind(this)
-
-
     }
-
-
-
-
-
-
-    GetRandP=()=>{
-
-        const nums = "0123456789"
-        const lower ="abcdefghijklmnopqrstuvwxyz"
-        const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        const symbols = "!@$%^&*()<>,.?/[]{}-=_+"
-
-        console.log(this.usenum.current.firstChild.firstChild.value)
-        var chars = nums + lower + upper + symbols
-        var passwordlen = 9
-        console.log("this button was pushed" )
-        var randpassword = ""
-        for (var i = 0; i<=passwordlen; i++){
-            var randnum = Math.floor(Math.random() * chars.length)
-            randpassword += chars.substring(randnum, randnum +1)
-        }
-        console.log(randpassword)
-    }
-
-
-
-
     render() {
-
-
-        // console.log(Auth.user.attributes)
-
-
     return (
       <div className="App">
         <AmplifySignOut />
         <Creator />
-
-      {/*  make component that lists all passwords in database*/}
       </div>
     );
   }
 }
 
-// <input type="text"/>
-// <input name="password"  type="text"/>
-//
-// <button onClick={this.ApiTest}>create random</button>
-// <button >add password</button>
-// <Myswitch ref={this.usenum}/>
-// <p>
-//   <button>show all saved passwords</button>
-// </p>
 
 
 
--- src/Creator.js
+++ src/Creator.js
@@ -6,8 +6,9 @@ export default function Creator(){
 
 
 
-    const [id, setid] = React.useState('')
+    const [objid, setid] = React.useState('')
     const [userid, setuser] = React.useState('')
+    const [dobject, setdel] = React.useState('')
     const [website, setwebsite] = React.useState('')
     const [password, setpassword] = React.useState('')
     const [sites, setsitepassword] = React.useState([])
@@ -18,31 +19,68 @@ export default function Creator(){
 
     const handlesubmit = e =>{
         e.preventDefault()
+        let randid = v4()
 
         API.post('cs453api5','/passwords',{
           body:{
-            id:v4(),
+            id:randid,
             site:website,
             password:password
           }
-        })
+        }).then(()=>setsitepassword([...sites, {
+          id:randid,
+          site:website,
+          password:password
+        } ]))
+      setpassword('')
+      setwebsite('')
     }
 
-    const handleDelete = (item) => {
-        console.log(item)
+
+    const handlerandpass = () =>{
+      const nums = "0123456789"
+      const lower ="abcdefghijklmnopqrstuvwxyz"
+      const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+      const symbols = "!@$%^&*()<>,.?/[]{}-=_+"
+
+
+      var chars = nums + lower + upper + symbols
+      var passwordlen = 9
+      console.log("this button was pushed" )
+      var randpassword = ""
+      for (var i = 0; i<=passwordlen; i++){
+        var randnum = Math.floor(Math.random() * chars.length)
+        randpassword += chars.substring(randnum, randnum +1)
+      }
+      console.log(randpassword)
+      setpassword(randpassword)
+    }
+
+    const handleDelete = (e, entryid) => {
+      e.preventDefault()
+
+      console.log(entryid)
+      API.del('cs453api5', '/passwords/object/:id', {
+        body:{
+          id:entryid
+        }
+      } ).then( ()=> setsitepassword([...sites.filter((I) => I.id !== entryid )]))
     }
   return (
       <div>
+        <h1>wellcome to the vault</h1>
 
           <form onSubmit={handlesubmit}>
             <input value={website} placeholder="ex. amazon, bliz.net, steam" onChange={(e) => setwebsite(e.target.value)}/>
             <input value={password} placeholder="ex.55#aCbobnotDead" onChange={(e) => setpassword(e.target.value)}/>
             <button> add password to vault</button>
           </form>
-          <table >
+        <button onClick={handlerandpass}> randompassword </button>
+
+          <table  >
               {sites.map(entry =>
                   <tr>
-                      <td>{entry.site}</td><td>{entry.password}</td><button onClick={() => handleDelete(entry)}>delete</button>
+                      <td>{entry.site}</td><td>{entry.password}</td><button onClick={(e) => {setdel(entry.id); handleDelete(e, entry.id)}}>delete</button>
                   </tr>
               )}
           </table>
